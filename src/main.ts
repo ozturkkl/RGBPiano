cca08cd97812c02eb0c927699e3ce200446c986c
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2023-05-13 18:34:06 -0500

    setup connection even name

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import WebSocket from "ws";
 (async () => {
   const ws = (await connect()) as WebSocket.WebSocket;
 
-  ws.on("message", (message) => {
+  ws.on("midi", (message) => {
     console.log(`Message received: ${message}`);
   });
+
+  // setInterval(() => {
+  //   ws.emit("midi", "Hello, world!");
+  // }, 1000);
 })();
