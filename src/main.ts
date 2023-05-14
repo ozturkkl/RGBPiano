12f44559fa05ef17e83cb3901210870d3ac72676
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2023-05-14 15:55:04 -0500

    setting up midi connection

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { Connection } from "./util/websocket";
+import { Midi } from "./util/midi";
 
 (async () => {
   const connection = new Connection();
   await connection.connect();
 
-  setInterval(() => {
-    connection.send("Hello, world!");
-  }, 2000);
-
-  connection.listen((message) => {
-    console.log(`Received message: ${message}`);
-  });
-  
+  const midi = new Midi(connection);
+  await midi.openInput();
 })();
---
+++ src/util/midi.ts
@@ -0,0 +1,76 @@
+import midi from "@julusian/midi";
+import readline from "readline";
+import { Connection } from "./websocket";
+
+export class Midi {
+  devices: string[] = [];
+
+  input: midi.Input;
+  output: midi.Output;
+
+  constructor(connection: Connection) {
+    this.input = new midi.Input();
+    this.output = new midi.Output();
+    this.devices = this.getDevices();
+
+    this.input.on("message", (deltaTime, message) => {
+      connection.send({
+        type: "midi",
+        data: {
+          deltaTime,
+          message,
+        },
+      });
+    });
+  }
+
+  getDevices() {
+    const devices = [];
+    for (let i = 0; i < this.input.getPortCount(); i++) {
+      devices.push(this.input.getPortName(i));
+    }
+    return devices;
+  }
+
+  async openInput(device?: string) {
+    if (device) {
+      this.getDevices().forEach((d, i) => {
+        if (d === device) {
+          try {
+            if (this.input.isPortOpen()) this.input.closePort();
+            this.input.openPort(i);
+            console.log(`Opened port ${d}`);
+          } catch (e) {
+            console.error(`Failed to open port ${d}: ${e}`);
+          }
+        }
+      });
+    } else {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      const devices = this.getDevices();
+      console.log("Available devices:");
+      devices.forEach((d, i) => {
+        console.log(`${i}: ${d}`);
+      });
+      console.log("Which device would you like to use?");
+      const answer = await new Promise<string>((resolve) => {
+        rl.question("Device: ", (answer) => {
+          resolve(answer);
+        });
+      });
+      rl.close();
+
+      try {
+        this.openInput(devices[parseInt(answer)]);
+      } catch (e) {
+        console.error(
+          `Failed to open port ${this.devices[parseInt(answer)]}: ${e}`
+        );
+      }
+    }
+  }
+}
