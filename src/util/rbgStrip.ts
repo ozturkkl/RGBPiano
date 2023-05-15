c4a7c3e505fce41c14927d8d2e3d232c432f1fad
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2023-05-14 23:19:00 -0500

    optimizing strip length

--- src/util/rbgStrip.ts
+++ src/util/rbgStrip.ts
@@ -1,7 +1,7 @@
 import ws281x from "rpi-ws281x-native";
 
 export class RgbStrip {
-  NUM_LEDS = 175; // Number of LEDs in the strip
+  NUM_LEDS = 177; // Number of LEDs in the strip
   BRIGHTNESS = 128; // The brightness level of the LEDs (0-255)
   DATA_PIN = 18; // The GPIO pin that the strip is connected to
 
@@ -25,7 +25,7 @@ export class RgbStrip {
       console.error("No pixel position provided for setPixelColor()");
       return;
     }
-    const pixel = Math.floor(pixelPositionPercent * (this.NUM_LEDS - 1)) + 1;
+    const pixel = Math.floor(pixelPositionPercent * (this.NUM_LEDS - 2)) + 2;
     this.colors[pixel] = (red << 16) | (green << 8) | blue;
     ws281x.render();
   }
