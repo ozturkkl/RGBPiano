1de23d9d5870709e656285afefa121c0df407b39
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2024-07-05 01:59:29 -0400

    fix ts error

--- src/main/util/colors.ts
+++ src/main/util/colors.ts
@@ -23,7 +23,7 @@ export function HSLToRGB(h: number, s: number, l: number): [number, number, numb
 
 export function hexToRgb(hex) {
   return hex
-    .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+    .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (_m, r, g, b) => '#' + r + r + g + g + b + b)
     .substring(1)
     .match(/.{2}/g)
     .map((x) => parseInt(x, 16))
